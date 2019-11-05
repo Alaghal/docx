fead18b626faa41f3042cd29213ccd820088c1cb
69SeryakovAV <SeryakovAV@cbr.ru>
2019-11-05 17:49:38 +0700

    add new functional

--- src/main/java/cbr/test/docx/DocxApplication.java
+++ src/main/java/cbr/test/docx/DocxApplication.java
@@ -16,9 +16,9 @@ import java.util.*;
 public class DocxApplication {
 
     public static void main(String[] args) throws IOException, Docx4JException, JAXBException {
-      //  SpringApplication.run(DocxApplication.class, args);
+        //SpringApplication.run(DocxApplication.class, args);
         testWorkTabl workTabl = new testWorkTabl();
-        WordprocessingMLPackage template =  workTabl.getTemplate("C:\\Users\\Серяковы\\Desktop\\template.docx");
+        WordprocessingMLPackage template =  workTabl.getTemplate("C:\\Users\\69seryakovav\\Desktop\\template.docx");
 
 
         List<SectionData> sectionDataList = new ArrayList<>();
@@ -43,8 +43,8 @@ public class DocxApplication {
         sectionDataList.add(new SectionData(Arrays.asList(repl1,repl2,repl3),null));
 
         workTabl.fillTemplateBySanctionsData(template,sectionDataList,"START_SECTIONS", "END_SECTION");
-
-        workTabl.writeDocxToStream(template,"C:\\Users\\Серяковы\\Desktop\\template2.docx");
+          List<Object> objectList = template.getMainDocumentPart().getContent();
+        workTabl.writeDocxToStream(template,"C:\\Users\\69seryakovav\\Desktop\\template2.docx");
         //workTabl.replaceTable(new String[]{"SJ_FUNCTION","SJ_DESC","SJ_PERIOD"}, Arrays.asList(repl1,repl2,repl3),template);
 
 
--- src/main/java/cbr/test/docx/services/Editors/docx/testWorkTabl.java
+++ src/main/java/cbr/test/docx/services/Editors/docx/testWorkTabl.java
@@ -2,11 +2,13 @@ package cbr.test.docx.services.Editors.docx;
 
 import org.docx4j.XmlUtils;
 import org.docx4j.jaxb.Context;
+import org.docx4j.jaxb.XPathBinderAssociationIsPartialException;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.wml.*;
 
 import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,131 +18,154 @@ import java.util.List;
 import java.util.Map;
 
 public class testWorkTabl {
-
-    public WordprocessingMLPackage getTemplate(String name) throws Docx4JException, FileNotFoundException {
-        WordprocessingMLPackage template = WordprocessingMLPackage.load(new FileInputStream(new File(name)));
-        // WordprocessingMLPackage template = WordprocessingMLPackage.load();
-        return template;
-    }
-
-    public static List<Object> getAllElementFromObject(Object obj, Class<?> toSearch) {
-        List<Object> result = new ArrayList<Object>();
-        if (obj instanceof JAXBElement) obj = ((JAXBElement<?>) obj).getValue();
-
-        if (obj.getClass().equals(toSearch))
-            result.add(obj);
-        else if (obj instanceof ContentAccessor) {
-            List<?> children = ((ContentAccessor) obj).getContent();
-            for (Object child : children) {
-                result.addAll(getAllElementFromObject(child, toSearch));
-            }
-
-        }
-        return result;
-    }
-
-
-    public void writeDocxToStream(WordprocessingMLPackage template, String target// OutputStream outputStream
-    ) throws IOException, Docx4JException {
-        // template.save(outputStream);
-        File f = new File(target);
-        template.save(f);
-    }
-
-
-    public void fillTemplateBySanctionsData(WordprocessingMLPackage template, List<SectionData> sectionDataListData, String startGenerateSectionPlaceholder, String endGenerateSectionPlaceholder) {
-
-        List<Object> elementDocument = template.getMainDocumentPart().getContent();
-        List<Object> elementForGenerate = new ArrayList();
-        int startIndexSection = getIndexTextElement(template, startGenerateSectionPlaceholder) + 1;
-        int endIndexSection = getIndexTextElement(template, endGenerateSectionPlaceholder);
-
-        for (int i = startIndexSection; i < endIndexSection; i++) {
-            elementForGenerate.add(XmlUtils.deepCopy(elementDocument.get(i)));
-        }
-
-        List<Object> resultList = new ArrayList();
-        for (SectionData sectionData : sectionDataListData) {
-            resultList.addAll(fillGeneratedSactions(elementForGenerate, sectionData.getDataToAddForParagraps(), sectionData.getDataToAddForTable()));
-        }
-
-        if (startIndexSection > 0 && !resultList.isEmpty()) {
-            template.getMainDocumentPart().getContent().addAll(startIndexSection, resultList);
-        }
-
-    }
-
-    private int getIndexTextElement(WordprocessingMLPackage template, String textForSearch) {
-        List<Object> texts = getAllElementFromObject(template.getMainDocumentPart(), Text.class);
-
-        for (Object text : texts) {
-            Text textElement = (Text) text;
-            if (textElement.getValue().equals(textForSearch)) {
-              return   template.getMainDocumentPart().getContent().indexOf(text);
-            }
-        }
-        return -1;
-    }
-
-    private List<Object> cloneList(List<Object> elementDocument) {
-        List<Object> cloneList = new ArrayList(elementDocument.size());
-        for (Object item : elementDocument) cloneList.add(XmlUtils.deepCopy(item));
-        return cloneList;
-    }
-
-    private List<Object> fillGeneratedSactions(List<Object> elementDocument, Map<String, String> dataForReplace, List<Map<String, String>> dataToAddForTable) {
-        List<Object> cloneElementDocument = cloneList(elementDocument);
-        for (int i = 0; i < cloneElementDocument.size(); i++) {
-            Object obj = cloneElementDocument.get(i);
-            if (obj instanceof JAXBElement) obj = ((JAXBElement<?>) obj).getValue();
-
-            if (obj.getClass().equals(Text.class)){
-                Text textElement = (Text) obj;
-                String replacementValue = dataForReplace.get(textElement.getValue());
-                if (replacementValue != null) {
-                    textElement.setValue(replacementValue);
-                }
-            }  else if (obj.getClass().equals(Tbl.class)) {
-                Tbl table = (Tbl) obj;
-                List<Object> rows = getAllElementFromObject(table, Tr.class);
-                for (int j = 1; j < rows.size(); j++) {
-                    Tr templateRow = (Tr) rows.get(j);
-
-                    for (Map<String, String> replacements : dataToAddForTable) {
-                        addRowToTable(table, templateRow, replacements);
-                    }
-
-                    table.getContent().remove(templateRow);
-                }
-            }
-        }
-
-        return cloneElementDocument;
-    }
-
-
-    public static void addRowToTable(Tbl reviewtable, Tr templateRow, Map<String, String> replacements) {
-        Tr workingRow = (Tr) XmlUtils.deepCopy(templateRow);
-        List textElements = getAllElementFromObject(workingRow, Text.class);
-        for (Object object : textElements) {
-            Text text = (Text) object;
-            String replacementValue = (String) replacements.get(text.getValue());
-            if (replacementValue != null)
-                text.setValue(replacementValue);
-        }
-
-        reviewtable.getContent().add(workingRow);
-    }
-
-    public P createPageBreak() {
-        Br br = Context.getWmlObjectFactory().createBr();
-        br.setType(STBrType.PAGE);
-
-        R run = Context.getWmlObjectFactory().createR();
-        run.getContent().add(br);
-
-        P paragraph = Context.getWmlObjectFactory().createP();
-        paragraph.getContent().add(run);
-        return paragraph;
-    }
+	
+	public WordprocessingMLPackage getTemplate(String name) throws Docx4JException, FileNotFoundException {
+		WordprocessingMLPackage template = WordprocessingMLPackage.load(new FileInputStream(new File(name)));
+		// WordprocessingMLPackage template = WordprocessingMLPackage.load();
+		return template;
+	}
+	
+	public static List<Object> getAllElementFromObject(Object obj, Class<?> toSearch) {
+		List<Object> result = new ArrayList<Object>();
+		if (obj instanceof JAXBElement) obj = ((JAXBElement<?>) obj).getValue();
+		
+		if (obj.getClass().equals(toSearch))
+			result.add(obj);
+		else if (obj instanceof ContentAccessor) {
+			List<?> children = ((ContentAccessor) obj).getContent();
+			for (Object child : children) {
+				result.addAll(getAllElementFromObject(child, toSearch));
+			}
+			
+		}
+		return result;
+	}
+	
+	
+	public void writeDocxToStream(WordprocessingMLPackage template, String target// OutputStream outputStream
+	) throws IOException, Docx4JException {
+		// template.save(outputStream);
+		File f = new File(target);
+		template.save(f);
+	}
+	
+	
+	public void fillTemplateBySanctionsData(WordprocessingMLPackage template, List<SectionData> sectionDataListData, String startGenerateSectionPlaceholder, String endGenerateSectionPlaceholder) throws JAXBException, XPathBinderAssociationIsPartialException {
+		
+		List<Object> elementDocument = template.getMainDocumentPart().getContent();
+		List<Object> elementForGenerate = new ArrayList();
+		int startIndexSection = getIndexTextElement(template, startGenerateSectionPlaceholder) + 1;
+		int endIndexSection = getIndexTextElement(template, endGenerateSectionPlaceholder);
+		
+		for (int i = startIndexSection; i < endIndexSection; i++) {
+			elementForGenerate.add(XmlUtils.deepCopy(elementDocument.get(i)));
+		}
+		
+		List<Object> resultList = new ArrayList();
+		for (SectionData sectionData : sectionDataListData) {
+			resultList.addAll(fillGeneratedSactions(elementForGenerate, sectionData.getDataToAddForParagraps(), sectionData.getDataToAddForTable()));
+		}
+		
+		if (startIndexSection > 0 && !resultList.isEmpty()) {
+			template.getMainDocumentPart().getContent().addAll(resultList);
+		}
+		
+	}
+	
+	private int getIndexTextElement(WordprocessingMLPackage template, String textForSearch) throws JAXBException, XPathBinderAssociationIsPartialException {
+		int index = -1;
+		String textNodesXPath = "//w:t";
+		List<Object> textNodes = template.getMainDocumentPart()
+				.getJAXBNodesViaXPath(textNodesXPath, false);
+		for (Object obj : textNodes) {
+			Text text = (Text) ((JAXBElement) obj).getValue();
+			String textValue = text.getValue();
+			
+			if (textValue.equals(textForSearch)) {
+				Object objparent = text.getParent();
+				if (objparent instanceof R) {
+					Object objectParent2 = ((R) objparent).getParent();
+					if (objectParent2 instanceof P) {
+						index = template.getMainDocumentPart().getContent().indexOf(objectParent2);
+						return index;
+					}
+					
+				}
+			}
+		}
+		
+		return index;
+	}
+	
+	private List<Object> cloneList(List<Object> elementDocument) {
+		List<Object> cloneList = new ArrayList(elementDocument.size());
+		for (Object item : elementDocument) cloneList.add(XmlUtils.deepCopy(item));
+		return cloneList;
+	}
+	
+	private List<Object> fillGeneratedSactions(List<Object> elementDocument, Map<String, String> dataForReplace, List<Map<String, String>> dataToAddForTable) {
+		List<Object> cloneElementDocument = cloneList(elementDocument);
+		for (int i = 0; i < cloneElementDocument.size(); i++) {
+			Object obj = cloneElementDocument.get(i);
+			
+			if (dataForReplace !=null && obj instanceof P) {
+				List<Object> paragraphs = ((P) obj).getContent();
+				for (Object paragrapsObject : paragraphs) {
+					if (paragrapsObject instanceof R) {
+						List<Object> objectList = ((R) paragrapsObject).getContent();
+						for (Object elementR : objectList) {
+							if (elementR instanceof Text) {
+								Text textElement = (Text) elementR;
+								String replacementValue = dataForReplace.get(textElement.getValue());
+								if (replacementValue != null) {
+									textElement.setValue(replacementValue);
+								}
+							}
+						}
+					}
+				}
+				
+			} else if (obj instanceof JAXBElement) {
+				Tbl table = (Tbl) ((JAXBElement) obj).getValue();
+				List<Object> rows = getAllElementFromObject(table, Tr.class);
+				for (int j = 0, k = 1; j < rows.size()-1; j++, k++) {
+					
+					Tr templateRow = (Tr) rows.get(k);
+					Map<String, String> replacements = dataToAddForTable.get(j);
+					
+					addRowToTable(table, templateRow, replacements);
+					
+					table.getContent().remove(templateRow);
+				}
+			}
+		}
+		
+		return cloneElementDocument;
+	}
+	
+	
+	public static void addRowToTable(Tbl reviewtable, Tr templateRow, Map<String, String> replacements) {
+		Tr workingRow = (Tr) XmlUtils.deepCopy(templateRow);
+		List textElements = getAllElementFromObject(workingRow, Text.class);
+		for (Object object : textElements) {
+			Text text = (Text) object;
+			String replacementValue = (String) replacements.get(text.getValue());
+			if (replacementValue != null)
+				text.setValue(replacementValue);
+		}
+		
+		reviewtable.getContent().add(workingRow);
+	}
+	
+	public P createPageBreak() {
+		Br br = Context.getWmlObjectFactory().createBr();
+		br.setType(STBrType.PAGE);
+		
+		R run = Context.getWmlObjectFactory().createR();
+		run.getContent().add(br);
+		
+		P paragraph = Context.getWmlObjectFactory().createP();
+		paragraph.getContent().add(run);
+		return paragraph;
+	}
 }
